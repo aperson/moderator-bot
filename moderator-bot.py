5bd8d7f8903c4c74f61c10b667e0a24d72732008
Zach McCullough <nosrepa@gmail.com>
2012-07-17 02:06:20 -0500

    updated free minecraft regex

--- moderator-bot.py
+++ moderator-bot.py
@@ -269,8 +269,8 @@ class FreeMinecraft(Filter):
     def __init__(self):
         Filter.__init__(self)
         self.regex = re.compile(r'''(?:free|cracked)?-?minecraft-?(?:install|codes?|rewards?|gif'''
-            r'''t-?codes?(?:-?generator)?|acc(?:t|ount)s?|now)(?:\.blogspot)?\.(?:me|info|com|net|'''
-            r'''org|ru|co\.uk|us)''', re.I)
+            r'''t-?codes?(?:-?generator)?|acc(?:t|ount)s?|now|forever)(?:\.blogspot)?\.(?:me|info|'''
+            r'''com|net|org|ru|co\.uk|us)''', re.I)
         self.action = 'spammed'
         self.ban = True
 
