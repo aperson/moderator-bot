ad5d5629a6a34c0b01ac6083fc68982331694d76
Zach McCullough <nosrepa@gmail.com>
2012-09-08 20:46:52 -0500

    And failed me again.  This is what I get for testing stuff in a separate file and copying it in.

--- moderator-bot.py
+++ moderator-bot.py
@@ -553,36 +553,36 @@ class YoutubeSpam(Filter):
         these all will count against the user and an overall score will be returned.  Also, we only
         check against the last 100 items on the user's profile.'''
 
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-agent', 'moderator-bot.py v2')]
-    with opener.open(
-        'http://www.reddit.com/user/{}/comments/.json?limit=100&sort=new'.format(user)) as w:
-        comments = json.loads(w.read().decode('utf-8'))['data']['children']
-        time.sleep(2)
-    with opener.open(
-        'http://www.reddit.com/user/{}/submitted/.json?limit=100&sort=new'.format(user)) as w:
-        submitted = json.loads(w.read().decode('utf-8'))['data']['children']
-        time.sleep(2)
-    video_count = 0
-    video_authors = set()
-    video_submissions = set()
-    comments_on_self = 0
-    for item in submitted:
-        item = item['data']
-        video_author = self._isVideo(item)
-        if video_author:
-            video_count += 1
-            video_authors.add(video_author)
-            video_submissions.add(item['name'])
-    for item in comments:
-        item = item['data']
-        if item['link_id'] in video_submissions:
-            comments_on_self += 1
-    if len(video_authors) == 1 and video_count >= 3:
-        if (((video_count + comments_on_self) / (len(comments) + len(submitted))) * 100) > 90:
-            return True
-        else:
-            return False
+        opener = urllib.request.build_opener()
+        opener.addheaders = [('User-agent', 'moderator-bot.py v2')]
+        with opener.open(
+            'http://www.reddit.com/user/{}/comments/.json?limit=100&sort=new'.format(user)) as w:
+            comments = json.loads(w.read().decode('utf-8'))['data']['children']
+            time.sleep(2)
+        with opener.open(
+            'http://www.reddit.com/user/{}/submitted/.json?limit=100&sort=new'.format(user)) as w:
+            submitted = json.loads(w.read().decode('utf-8'))['data']['children']
+            time.sleep(2)
+        video_count = 0
+        video_authors = set()
+        video_submissions = set()
+        comments_on_self = 0
+        for item in submitted:
+            item = item['data']
+            video_author = self._isVideo(item)
+            if video_author:
+                video_count += 1
+                video_authors.add(video_author)
+                video_submissions.add(item['name'])
+        for item in comments:
+            item = item['data']
+            if item['link_id'] in video_submissions:
+                comments_on_self += 1
+        if len(video_authors) == 1 and video_count >= 3:
+            if (((video_count + comments_on_self) / (len(comments) + len(submitted))) * 100) > 90:
+                return True
+            else:
+                return False
 
     def filterSubmission(self, submission):
         DAY = 24 * 60 * 60
