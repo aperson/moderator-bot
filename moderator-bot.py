be8e67c62cbe45a21b73e56722b125ce7b7ee2da
Zach McCullough <nosrepa@gmail.com>
2012-09-12 10:05:50 -0500

    forgot a : :(

--- moderator-bot.py
+++ moderator-bot.py
@@ -349,7 +349,7 @@ class ServerAd(Filter):
         image_list = []
 
         if url.startswith('a/'):
-            with self.opener.open('http://api.imgur.com/2/album/{}.json'.format(url[2])) as w:
+            with self.opener.open('http://api.imgur.com/2/album/{}.json'.format(url[2:])) as w:
                 imgur = json.loads(w.read().decode('utf-8'))['album']
             image_list.append({'title': imgur['title'], 'caption': imgur['description']})
             for i in imgur['images']:
