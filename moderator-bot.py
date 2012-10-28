df1810997663789bfad17443f6c2690c4a090c7d
Zach McCullough <nosrepa@gmail.com>
2012-10-28 18:31:49 -0500

    added too late to inane title

--- moderator-bot.py
+++ moderator-bot.py
@@ -895,7 +895,7 @@ class InaneTitle(Filter):
         self.regex = re.compile(
             r'''you(?:'?re|r| are) drunk|minecraft logic|seems legit|'''
             r'''what does (?:/?r/minecraft|reddit) think|yo,? d(?:o|aw)g|'''
-            r'''^\.*?(?:too )?soon[.?]*?$|am i the only(?: one)?|you had one job''',
+            r'''^\.*?(?:too )?(?:soon|late)[.?]*?$|am i the only(?: one)?|you had one job''',
             re.I)
         self.comment_template = (
             """Hey there, you seem to be using an inane title!  You can pro"""
