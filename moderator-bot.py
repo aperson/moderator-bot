630943e6a878d4a1ca0d8ca183955b751f60d696
Zach McCullough <nosrepa@gmail.com>
2012-12-02 12:34:08 -0600

    this is what I get for not paying attention

--- moderator-bot.py
+++ moderator-bot.py
@@ -93,10 +93,10 @@ def mojangStatus():
         return None
     text = []
     for i in ('website', 'login', 'account', 'session', 'skins'):
-        if status[i]['status'].startswith('Online'):
+        if status[i]['status'].startswith('up'):
             text.append("[{server} is online](#status_green '{status}')".format(
                 server=i, status=status[i]['title']))
-        elif status[i]['status'].startswith('Server Error'):
+        elif status[i]['status'].startswith('down'):
             text.append("[{server} is offline](#status_red '{status}')".format(
                 server=i, status=status[i]['title']))
     return ''.join(text) + '\n\n^server ^status ^provided ^by ^[xpaw.ru](http://xpaw.ru/mcstatus)'
