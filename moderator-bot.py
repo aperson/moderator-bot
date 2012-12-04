38385a27d9c724256e9585567c03056fc5c41735
Zach McCullough <nosrepa@gmail.com>
2012-12-04 10:25:03 -0600

    make the text a bit more readable for people with styles off, I'll probably change this to not use strikethrough later on

--- moderator-bot.py
+++ moderator-bot.py
@@ -94,14 +94,14 @@ def mojangStatus():
     text = []
     for i in ('website', 'login', 'account', 'session', 'skins'):
         if status[i]['status'] == 'up':
-            text.append("[{server} is online](#status_green '{server} - {status}')".format(
+            text.append("[{server} is online.  ](#status_green '{server} - {status}')".format(
                 server=i.title(), status=status[i]['title'].split()[0]))
         elif status[i]['status'] == 'problem':
             text.append(
-                "[{server} is experiencing a problem](#status_green '{server} - {status}')".format(
+                "[{server} is having a problem.  ](#status_green '{server} - {status}')".format(
                     server=i.title(), status=status[i]['title']))
         elif status[i]['status'] == 'down':
-            text.append("[{server} is offline](#status_red '{server} - {status}')".format(
+            text.append("[{server} is offline.  ](#status_red '{server} - {status}')".format(
                 server=i.title(), status=status[i]['title']))
     return ''.join(text)
 
