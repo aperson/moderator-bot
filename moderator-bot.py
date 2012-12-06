4e952e71ca3eb7291023c7d8eeac6e29317d3b99
Zach McCullough <nosrepa@gmail.com>
2012-12-06 16:58:55 -0600

    added 'when suddenly' to inane filter

--- moderator-bot.py
+++ moderator-bot.py
@@ -922,7 +922,7 @@ class InaneTitle(Filter):
             r'''you(?:'?re|r| are) drunk|minecraft logic|seems legit|'''
             r'''what does (?:/?r/minecraft|reddit) think|yo,? d(?:o|aw)g|'''
             r'''^\.*?(?:too )?(?:soon|late)[.!?]*?$|am i the only(?: one)?|you had one job|'''
-            r'''^\S*ception$''',
+            r'''^\S*ception$|when suddenly''',
             re.I)
         self.comment_template = (
             """Hey there, you seem to be using an inane title!  You can pro"""
