7e9eea3489fb79f88085a61318d49ba1c256a151
Zach McCullough <nosrepa@gmail.com>
2013-01-02 15:29:33 -0600

    updated inane regex

--- moderator-bot.py
+++ moderator-bot.py
@@ -964,7 +964,7 @@ class InaneTitle(Filter):
             r'''you(?:'?re|r| are) drunk|minecraft logic|seems legit|'''
             r'''what does (?:/?r/minecraft|reddit) think|yo,? d(?:o|aw)g|'''
             r'''^\.*?(?:too )?(?:soon|late)[.!?]*?$|am i the only(?: one)?|you had one job|'''
-            r'''^\S*ception$|when suddenly|first post''',
+            r'''^\S*ception$|when suddenly|first post|am i doin(?g|')? this this ri(?:te|ght)''',
             re.I)
         self.comment_template = (
             """Hey there, you seem to be using an inane title!  You can pro"""
