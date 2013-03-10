aa3b3d0dbd1d304b13b840e680a0a91b1e5b87c2
Zach McCullough <nosrepa@gmail.com>
2013-03-09 19:20:56 -0600

    updated free minecraft regex

--- moderator-bot.py
+++ moderator-bot.py
@@ -621,7 +621,8 @@ class FreeMinecraft(Filter):
         self.regex = re.compile(
             r'''(?:(free|cracked)?-?minecraft-?(install|get|'''
             r'''(?:gift-?)?codes?(?:-?gen(?:erator)?)?|rewards?|acc(?:t|ount)s?(?:free)?|now|'''
-            r'''forever)?(?:\.blogspot)?|epicfreeprizes)\.(?:me|info|com|net|org|ru|co\.uk|us)''',
+            r'''forever)?(?:\.blogspot)?|epicfreeprizes)[\[\(\{\.]*[\]\)\}]*?'''
+            r'''(?:me|info|com|net|org|ru|co\.uk|us)''',
             re.I)
         self.action = 'spammed'
         self.ban = True
