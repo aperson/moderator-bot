5e353180df6ba9c54dce0f2086faeea3971306f8
Zach McCullough <nosrepa@gmail.com>
2014-02-13 15:37:01 -0600

    enable BannedYoutubers

--- moderator-bot.py
+++ moderator-bot.py
@@ -1225,11 +1225,12 @@ def main():
     p('Started monitoring submissions on /r/{}.'.format(SUBREDDIT))
 
     filters = [
-        Flair(r), Suggestion(), Fixed(), ServerAd(r, imgur, Youtube(cache_time=60*30)),
-        FreeMinecraft(), AmazonReferral(), ShortUrl(), Failed(), Minebook(), SelfLinks(),
-        BadWords(), YoutubeSpam(r, Youtube(cache_time=0)), BannedSubs(), Meme(),
-        InaneTitle(), SpamNBan(), AllCaps(), FileDownload(), ChunkError(), Facebook(), Reditr(),
-        YoutubeVideo(Youtube()), Twitch()]
+        Flair(r), BannedYoutubers(r, Youtube(cache_time=0)), Suggestion(), Fixed(),
+        ServerAd(r, imgur, Youtube(cache_time=60*30)), FreeMinecraft(), AmazonReferral(),
+        ShortUrl(), Failed(), Minebook(), SelfLinks(), BadWords(),
+        YoutubeSpam(r, Youtube(cache_time=0)), BannedSubs(), Meme(), InaneTitle(), SpamNBan(),
+        AllCaps(), FileDownload(), ChunkError(), Facebook(), Reditr(), YoutubeVideo(Youtube()),
+        Twitch()]
 
     # main loop
     while True:
